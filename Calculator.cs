8b41ab20d545f5d644502ecda6672f0f9f941021
Filip Fedor <xfedor11@vutbr.cz>
2021-04-08 16:12:06 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv09
+{
+    class Calculator
+    {
+
+        public string Display { get; private set; }
+        public string Memory { get; private set; }
+
+        private double operand1;
+        private double operand2;
+
+        private enum Operand { add, subtract, times, divide, equals };
+
+        private Operand _operand;
+        private CultureInfo myCulture;
+
+        public Calculator()
+        {
+            myCulture = new CultureInfo("cs-cz");
+            _operand = Operand.equals;
+            operand1 = 0;
+            Display = "0";
+            Memory = "0";
+        }
+
+
+        public void Button(string pressedButton)
+        {
+
+        }
+
+        public void NumberButton(string pressedButton)
+        {
+            if (pressedButton == "," && Display == "") Display = "0";
+            if (Display != "" && !Char.IsDigit(Display[0])) Display = "";
+            if ( Display == "0"&& pressedButton != ",") Display = pressedButton;
+            else if((!Display.Contains(",")&& pressedButton == ",")|| pressedButton != ",") Display += pressedButton;
+             
+        }
+
+        public void ClearButton(string pressedButton)
+        {
+            switch (pressedButton)
+            {
+                case ("C"):
+                    Display = "";
+                    break;
+                case ("CE"):
+                    Display = "";
+                    _operand = Operand.equals;
+                    break;
+                case ("<="):
+                    if (Display.Length > 0) Display = Display.Remove(Display.Length - 1);
+                    break;
+            }
+        }
+
+        public void Evaluate()
+        {
+            operand2 = double.Parse(Display, myCulture);
+            switch (_operand)
+            {
+                case Operand.equals:
+                    break;
+                case Operand.add:
+                    Display = "" + (operand1 + operand2);
+                    break;
+                case Operand.subtract:
+                    Display = "" + (operand1 - operand2);
+                    break;
+                case Operand.times:
+                    Display = "" + (operand1 * operand2);
+                    break;
+                case Operand.divide:
+                    if (operand2 == 0) Display = "Cannot divide by 0";
+                    else Display = "" + (operand1 / operand2);
+                    break;
+            }
+            _operand = Operand.equals;
+        }
+        public void OperationSet(string pressedButton)
+        {
+            operand1 = double.Parse(Display, myCulture);
+
+            switch (pressedButton)
+            {
+                case "+":
+                    _operand = Operand.add;
+                    Display = "+";
+                    break;
+                case "-":
+                    _operand = Operand.subtract;
+                    Display = "-";
+                    break;
+                case "*":
+                    _operand = Operand.times;
+                    Display = "*";
+                    break;
+                case "/":
+                    _operand = Operand.divide;
+                    Display = "/";
+                    break;
+                case "X*X":
+                    Display = "" + (operand1 * operand1);
+                    break;
+            }
+        }
+        public void MemoryOperation(string pressedButton)
+        {
+            switch (pressedButton)
+            {
+                case "MC":
+                    Memory = "0";
+                    break;
+                case "MS":
+                    if (Display != "" && Char.IsDigit(Display[0])) Memory = Display;
+                    break;
+                case "MR":
+                    Display = Memory;
+                    break;
+                case "M+":
+                    if (Display != "" && Char.IsDigit(Display[0]))
+                    {
+                        Memory = "" + (double.Parse(Memory, myCulture) + double.Parse(Display, myCulture));
+                    }
+                    break;
+                case "M-":
+                    if (Display != "" && Char.IsDigit(Display[0]))
+                    {
+                        Memory = "" + (double.Parse(Memory, myCulture) - double.Parse(Display, myCulture));
+                    }
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cv09
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator calculator = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private void Update()
+        {
+            display.Text= calculator.Display;
+            memory.Text= calculator.Memory;
+        }
+
+        private void Button_Number_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.NumberButton((sender as Button).Content.ToString());
+            Update();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Button((sender as Button).Content.ToString());
+            Update();
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.ClearButton((sender as Button).Content.ToString());
+            Update();
+        }
+
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Evaluate();
+            Update();
+        }
+
+        private void operation_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.OperationSet((sender as Button).Content.ToString());
+            Update();
+        }
+
+        private void MemoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.MemoryOperation((sender as Button).Content.ToString());
+            Update();
+        }
+    }
+}
